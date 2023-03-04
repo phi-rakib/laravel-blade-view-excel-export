81014a2bcc3730fd7de39a0bc86070457a011492
rakib <phi.rakib@gmail.com>
2023-03-04 19:17:51 +0600

    added routes for student index & export

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::controller(StudentController::class)->group(function() {
+    Route::get('students', 'index');
+    Route::get('students/export', 'export')->name('students.export');
+    Route::get('students/table/export', 'exportTable')->name('students.table.export');
 });
